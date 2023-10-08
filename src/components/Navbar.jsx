34a97e63252c7d9d44c532f0273164510b30302a
RayanEreteo <rayabf5@gmail.com>
2023-10-08 21:51:47 +0200

    Navbar terminée

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -5,7 +5,15 @@ import "../css/Navbar.css"
 function Navbar() {
   return (
     <div className='navbar'>
-
+        <div className="link-container">
+            <a href="" className='link'>Introduction</a>
+            <div className="separator" style={{width: '20px', height: "100%", background: "#D9D9D9"}}></div>
+            <a href="" className='link'>Mon Parcours</a>
+            <div className="separator" style={{width: '20px', height: "100%", background: "#D9D9D9"}}></div>
+            <a href="" className='link'>Portfolio</a>
+            <div className="separator" style={{width: '20px', height: "100%", background: "#D9D9D9"}}></div>
+            <a href="" className='link'>Contact</a>
+        </div>
     </div>
   )
 }
