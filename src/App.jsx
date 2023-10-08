de83bd1902e01520a3bee49f8e2bd3723447a7d0
RayanEreteo <rayabf5@gmail.com>
2023-10-08 21:22:15 +0200

    début de la page d'introduction

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import Introduction from './components/Introduction'
+import Navbar from './components/Navbar'
+import './css/app.css'
+
+function App() {
+
+  return (
+    <>
+      <Navbar />
+      <Introduction />
+    </>
+  )
+}
+
+export default App
---
+++ src/components/Introduction.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+import '../css/Introduction.css'
+
+function Introduction() {
+  return (
+    <div className='introduction'>
+        <p>test</p>
+    </div>
+  )
+}
+
+export default Introduction
