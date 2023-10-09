0b982b8064e1825b1ffbf6224da2b6a8e5f039a0
RayanEreteo <rayabf5@gmail.com>
2023-10-09 15:12:49 +0200

    Avancé page d'intro -besoin de créer la ligne

--- src/components/Introduction.jsx
+++ src/components/Introduction.jsx
@@ -1,13 +1,36 @@
-import React from 'react'
+import "../css/Introduction.css";
 
-import '../css/Introduction.css'
+// Logo stack import
+import Mongo from "../assets/languagesLogo/mongo.svg";
+import Express from "../assets/languagesLogo/express.svg";
+import Node from "../assets/languagesLogo/node.svg";
+import React from "../assets/languagesLogo/react.svg";
 
 function Introduction() {
   return (
-    <div className='introduction'>
-        <p>test</p>
+    <div className="introduction">
+      <div className="main-container">
+        <h2 className="main-title">RAYAN ERETEO</h2>
+        <p className="catchword">Un développeur web full-stack</p>
+        <div className="stack-container">
+          <img className="stack-logo" src={Mongo} alt="MongoDB" />
+          <img
+            className="stack-logo"
+            src={Express}
+            alt="express"
+            // changement de la couleur du logo express
+            style={{
+              filter:
+                "invert(100%) sepia(0%) saturate(7500%) hue-rotate(318deg) brightness(104%) contrast(104%)",
+            }}
+          />
+          <img className="stack-logo" src={React} alt="react" />
+          <img className="stack-logo" src={Node} alt="node"  style={{width: '120px', height: '120px'}}/>
+        </div>
+        <h1 className="stack-name">M.E.R.N</h1>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Introduction
+export default Introduction;
