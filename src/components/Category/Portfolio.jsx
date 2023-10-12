e1cf97be691368c7c9e84c1fbf79e806c255ad05
RayanEreteo <rayabf5@gmail.com>
2023-10-12 19:21:54 +0200

    ajout des liens

--- src/components/Category/Portfolio.jsx
+++ src/components/Category/Portfolio.jsx
@@ -21,8 +21,8 @@ function Portfolio() {
               "Un jeu où vous devez presser la barre espace pour augmenter votre score. 3 power-ups sont disponibles pour vous aider à augmenter votre score le plus vite possible."
             }
             thumbnail={Clicker}
-            link={"https://youtube.com"}
-            sourceCodeLink={""}
+            link={"https://spacebar-clicker-23fc2.web.app"}
+            sourceCodeLink={"https://github.com/RayanEreteo/ClickerGame"}
           />
            <ProjectCard
             name={"Calculatrice"}
@@ -30,8 +30,8 @@ function Portfolio() {
               "Une calculatrice au design simple."
             }
             thumbnail={Calc}
-            link={"https://youtube.com"}
-            sourceCodeLink={""}
+            link={"https://reactcalculator-3f724.web.app"}
+            sourceCodeLink={"https://github.com/RayanEreteo/react-calculator"}
           />
            <ProjectCard
             name={"Génerateur de chat"}
@@ -39,8 +39,8 @@ function Portfolio() {
               "Une simple application ou en cliquant sur un boutton, vous générer une image de chat."
             }
             thumbnail={Cat}
-            link={"https://youtube.com"}
-            sourceCodeLink={""}
+            link={"https://reactcatgenerator.web.app"}
+            sourceCodeLink={"https://github.com/RayanEreteo/ReactCatGenerator"}
           />
         </div>
       </div>
