43e910a0b45090304f794f1b304657832d1a43c8
RayanEreteo <rayabf5@gmail.com>
2023-10-13 16:24:40 +0200

    Hosting première version. - Ajout du media query

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
 import Introduction from './components/Category/Introduction.jsx'
 import Navbar from './components/Navbar.jsx'
 import Parcours from './components/Category/Parcours.jsx'
+
 import './css/app.css'
+import './css/MediaQuery.css'
+
 import Portfolio from './components/Category/Portfolio.jsx'
 import Contact from './components/Category/Contact.jsx'
 
